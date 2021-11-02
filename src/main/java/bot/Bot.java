a2f8e7ec4b866248ab2f733b9d42e6ae214b9186
Yuliya Kovalenko <kja68@ya.ru>
2021-11-02 12:40:49 +0500

    Контроль тренировки
    добавила класс пользователя, у бота ест словарь пользователей так  каждого пользователя свою тренировочное состояние, по которому бот может вести тренировки параллельно

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -21,20 +21,31 @@ import java.util.*;
  * @author Ксения Шорохова
  */
 public class Bot extends TelegramLongPollingBot {
-
-    /** Поле имя бота - username в телеграме */
+    /**
+     *  Поле имя бота - username в телеграме
+     */
     @Setter
     @Getter
     private String botName;
 
-    /** Поле токен бота в телеграме для контроля над ботом */
+    /**
+     *  Поле токен бота в телеграме для контроля над ботом
+     */
     @Setter
     private String botToken;
 
-    /** Поле сборщика тренировки */
+    /**
+     *  Поле сборщик тренировки
+     */
     private WorkoutMaker workoutMaker = new WorkoutMaker();
 
-    /** Конструктор - создание нового объекта с определенными значениями
+    /**
+     * Поле словарь пользователей (ключ - id чата, значение - экземпляр класса пользователь)
+     */
+    private HashMap<String, User> users = new HashMap<>();
+
+    /**
+     *  Конструктор - создание нового объекта с определенными значениями
      * @param botName - username бота в телеграме
      * @param botToken - токен бота в телеграме
      */
@@ -43,19 +54,25 @@ public class Bot extends TelegramLongPollingBot {
         this.botToken = botToken;
     }
 
-    /** Функция получения значения поля {@link Bot#botName} */
+    /**
+     *  Функция получения значения поля {@link Bot#botName}
+     */
     @Override
     public String getBotUsername() {
         return botName;
     }
 
-    /** Фнкция получения значения поля {@link Bot#botToken} */
+    /**
+     *  Функция получения значения поля {@link Bot#botToken}
+     */
     @Override
     public String getBotToken() {
         return botToken;
     }
 
-    /** Процедура обработки обновлений */
+    /**
+     *  Процедура обработки обновлений
+     */
     @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
@@ -80,10 +97,11 @@ public class Bot extends TelegramLongPollingBot {
     /** Процедура обработки сообщений пользователя, содержащих команду
      * @param message - сообщение пользователя
      * @param commandEntity - команда
-     * */
+     */
     @SneakyThrows
     private void handleCommandMessage(Message message, Optional<MessageEntity> commandEntity) {
         String command = message.getText().substring(commandEntity.get().getOffset(), commandEntity.get().getLength());
+        String chatId = message.getChatId().toString();
         switch (command) {
             case "/help":
                 StringBuilder botMessage = new StringBuilder();
@@ -91,13 +109,12 @@ public class Bot extends TelegramLongPollingBot {
                 botMessage.append("придумана японским физиологом Изуми Табата. Он доказал, что интервальные тренировки при мощности работы на 70% от ");
                 botMessage.append("МПК (максимального потребления кислорода) способны одновременно привести к росту аэробной и анаэробной выносливости. ");
                 botMessage.append("Упражнения выполняются циклами. 1 цикл: 20 секунд работы, 10 секунд отдыха, 8 подходов. Количество пражнений в 1 раунде = колчество циклов. ");
-                botMessage.append("Я составлю тебе тренировку по выбранным тобою параметрам (уровень сложности, целевая группа мышц) Пиши /start_training, чтобы начать!");
-                execute(SendMessage.builder()
-                        .text(botMessage.toString())
-                        .chatId(message.getChatId().toString())
-                        .build());
+                botMessage.append("Я составлю тебе тренировку по выбранным тобою параметрам (уровень сложности, целевая группа мышц) Пиши /start, чтобы начать!");
+                sendTextMessage(botMessage.toString(), chatId);
                 break;
-            case "/start_training":
+            case "/start":
+                users.put(chatId, new User(chatId));
+                users.get(chatId).setBot(this);
                 botMessage = new StringBuilder();
                 botMessage.append("Выберите уровень сложности\nновичок : 1 раунд 6 циклов\nлюбитель : 2 раунда по 8 циклов\n");
                 botMessage.append("продвинутый : 3 раунда по 8 циклов");
@@ -106,11 +123,7 @@ public class Bot extends TelegramLongPollingBot {
                 for (String level : levels){
                     buttons.add(Arrays.asList(InlineKeyboardButton.builder().text(level).callbackData("chosen level: " + level).build()));
                 }
-                execute(SendMessage.builder()
-                        .text(botMessage.toString())
-                        .chatId(message.getChatId().toString())
-                        .replyMarkup(InlineKeyboardMarkup.builder().keyboard(buttons).build())
-                        .build());
+                sendMessageWithButtons(botMessage.toString(), chatId, buttons);
                 break;
             default:
                 handleUnclearMessage(message);
@@ -120,24 +133,25 @@ public class Bot extends TelegramLongPollingBot {
 
     /**Процедура обработки непонятных боту сообщений
      * @param message - сообщение пользователя
-     * */
+     */
     @SneakyThrows
     private void handleUnclearMessage(Message message){
-        execute(SendMessage.builder()
-                .text("Извините, я вас не понял :(, попробуйте еще раз")
-                .chatId(message.getChatId().toString())
-                .build());
+        sendTextMessage("Извините, я вас не понял :(, попробуйте еще раз", message.getChatId().toString());
     }
 
     /**Процедура обработки нажатия на кнопки
      * @param callbackQuery - обратный вызов
-     * */
+     */
     @SneakyThrows
     private void handleCallback(CallbackQuery callbackQuery){
         Message message = callbackQuery.getMessage();
         String[] params = callbackQuery.getData().split(": ");
         String name = params[0];
-        String  value = params[1];
+        String value = "";
+        if(params.length == 2) {
+            value = params[1];
+        }
+        String chatId = message.getChatId().toString();
         switch (name){
             case "chosen level":
                 List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
@@ -146,29 +160,83 @@ public class Bot extends TelegramLongPollingBot {
                 for (String group : groups){
                     buttons.add(Arrays.asList(InlineKeyboardButton.builder().text(group).callbackData("chosen group: " + group).build()));
                 }
-                workoutMaker.setLevel(value);
-                execute(SendMessage.builder()
-                        .text("Вы выбрали уровень " + value)
-                        .chatId(message.getChatId().toString())
-                        .build());
-                execute(SendMessage.builder()
-                        .text("Выберите целевую группу мышц")
-                        .chatId(message.getChatId().toString())
-                        .replyMarkup(InlineKeyboardMarkup.builder().keyboard(buttons).build())
-                        .build());
+                users.get(chatId).getWorkoutMaker().setLevel(value);
+                sendTextMessage("Вы выбрали уровень " + value, chatId);
+                sendMessageWithButtons("Выберите целевую группу мышц", chatId, buttons);
                 break;
             case "chosen group":
-                execute(SendMessage.builder()
-                        .text("Вы выбрали следующую группу мышц: " + value)
-                        .chatId(message.getChatId().toString())
-                        .build());
-                workoutMaker.setTargetGroups(value.split(", "));
-                ArrayList<String> workout = workoutMaker.createWorkout();
-                String strWorkout = workoutMaker.getStringWorkout(workout);
-                execute(SendMessage.builder()
-                        .text(strWorkout)
-                        .chatId(message.getChatId().toString())
-                        .build());
+                sendTextMessage("Вы выбрали следующую группу мышц: " + value, chatId);
+                users.get(chatId).getWorkoutMaker().setTargetGroups(value.split(", "));
+                buttons = new ArrayList<>();
+                buttons.add(Arrays.asList(InlineKeyboardButton.builder().text("начать").callbackData("start workout").build(),
+                        InlineKeyboardButton.builder().text("отменить").callbackData("cancel").build()));
+                sendMessageWithButtons("Начать тренировку ?", chatId, buttons);
+                break;
+            case "start workout":
+                users.get(chatId).setWorkout(users.get(chatId).getWorkoutMaker().createWorkout());
+                buttons = new ArrayList<>();
+                buttons.add(Arrays.asList(InlineKeyboardButton.builder().text("начать").callbackData("start round").build(),
+                        InlineKeyboardButton.builder().text("завершить тренировку").callbackData("stop").build()));
+                sendMessageWithButtons("Начать " + users.get(chatId).getCurrentRound() + " раунд?",
+                        chatId, buttons);
+                break;
+            case "start round":
+                buttons = new ArrayList<>();
+                buttons.add(Arrays.asList(InlineKeyboardButton.builder().text("начать").callbackData("start exercise").build(),
+                        InlineKeyboardButton.builder().text("завершить тренировку").callbackData("stop").build()));
+                sendMessageWithButtons((users.get(chatId).getCurrentExercise() + 1) + " упражнение: "+
+                        users.get(chatId).getExerciseName() + " Начать ?", chatId, buttons);
+                break;
+            case "start exercise":
+                buttons = new ArrayList<>();
+                buttons.add(Arrays.asList(InlineKeyboardButton.builder().text("начать").callbackData("start approach").build(),
+                        InlineKeyboardButton.builder().text("завершить тренировку").callbackData("stop").build()));
+                sendMessageWithButtons("Начать " + users.get(chatId).getCurrentApproach() + " подход?",
+                        chatId, buttons);
+                break;
+
+            case "start approach":
+                sendTextMessage("Paботаем!", chatId);
+                users.get(chatId).doExercise();
+                break;
+            case "start rest":
+                sendTextMessage("Отдых!", chatId);
+                users.get(chatId).rest(Integer.parseInt(value));
+                break;
+            case "cancel":
+                sendTextMessage("Тренировка отменена!", chatId);
+                break;
+            case "stop":
+                sendTextMessage("Тренировка завершена!", chatId);
+                break;
         }
+    }
+
+    /**
+     * Процедура отправки пользователю текстового сообщения
+     * @param text - текст сообщения
+     * @param chatId - ID чата, в который нужно отправить сообщение
+     */
+    @SneakyThrows
+    public void sendTextMessage(String text, String chatId){
+        execute(SendMessage.builder()
+                .text(text)
+                .chatId(chatId)
+                .build());
+    }
+
+    /**
+     * Процедура отправки пользователю сообщения с кнопками для ответа
+     * @param text - текст сообщения
+     * @param chatId - ID чата, в который нужно отправить сообщение
+     * @param buttons - список кнопок
+     */
+    @SneakyThrows
+    public void sendMessageWithButtons(String text, String chatId, List<List<InlineKeyboardButton>> buttons){
+        execute(SendMessage.builder()
+                .text(text)
+                .chatId(chatId)
+                .replyMarkup(InlineKeyboardMarkup.builder().keyboard(buttons).build())
+                .build());
     }
 }
